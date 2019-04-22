d461791594320ae4404a0e3907ce6d6983612598
Mohit Arya <43243827+Mohit-Arya1211@users.noreply.github.com>
2019-04-22 14:09:40 +0530

    Updated App.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 const schema = require('graphql-scraper')
-
 const server = new GraphQLServer({ schema })
 server.start(({ port }) => {
   console.log(
